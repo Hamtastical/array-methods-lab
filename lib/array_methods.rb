0b1fea96dc3eff85ad842eeaea0506f426247e3c
Hamtastical <meiliham1@gmail.com>
2021-02-02 11:41:27 -0500

    commit tree 7cab8e33bae7098d2f430abf51ef04bb8d8c88fa

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,32 @@
 def using_include(array, element)
+        array.include?(element)
 
 end
 
 def using_sort(array)
+    array.sort
 
 end
 
 def using_reverse(array)
+    array.reverse
 
 end
 
 def using_first(array)
+    array = ["wow","i"]
+    array.first
 
 end
 
 def using_last(array)
+    array = ["It","is","arrays!"]
+    array.last
 
 end
 
 def using_size(array)
+    array = ["I","am","here","today","working","hard"]
+    array.size
 
 end
